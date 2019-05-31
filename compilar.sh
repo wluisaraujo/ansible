ba905bf5da0d82b8df1ee51e96b38e47cc488256
W. Luís Araújo <w.luis.araujo@gmail.com>
2019-05-31 16:09:00 -0300

    Update compilar.sh

--- compilar.sh
+++ compilar.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 yum groupinstall "Development tools" -y
 # Estava no slide
-yum install libpcap-devel libmaxminddb-devel libsqlite3x-devel mariadb-devel rrdtool-devel redis -y
+yum install -y libpcap-devel libmaxminddb-devel libsqlite3x-devel mariadb-devel rrdtool-devel redis
 # Esta no docs
-yum install git autoconf automake autogen bison flex libpcap-devel libmaxminddb-devel hiredis-devel redis glib2-devel libxml2-devel sqlite-devel gcc-c++ libtool wget libcurl-devel pango-devel cairo-devel libpng-devel mysql-devel libnetfilter_queue-devel zlib-devel which libcap-devel readline-devel
+yum install -y git autoconf automake autogen bison flex libpcap-devel \ 
+libmaxminddb-devel hiredis-devel redis glib2-devel libxml2-devel sqlite-devel \ 
+gcc-c++ libtool wget libcurl-devel pango-devel cairo-devel libpng-devel mysql-devel \ 
+libnetfilter_queue-devel zlib-devel which libcap-devel readline-devel
 #
 cd /root
 git clone https://github.com/ntop/nDPI.git
